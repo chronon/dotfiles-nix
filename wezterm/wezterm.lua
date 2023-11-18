d8241e05c9718ed30cc4d8c46cb37db4930722cc
Gregory Gaskill <gregory@chronon.com>
2023-11-18 09:18:17 -0500

    Combine wezterm config into single file

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,9 +1,63 @@
-local utils = require("utils")
-local config = require("config")
-local keys = require("keys")
+local wezterm = require("wezterm")
+local act = wezterm.action
+local home = os.getenv("HOME")
+local mux = wezterm.mux
 
-require("startup")
+local config = {
+  font_size = 15,
+  default_prog = { home .. "/.nix-profile/bin/fish", "-l" },
+  initial_rows = 100,
+  initial_cols = 170,
+  window_decorations = "RESIZE",
+  force_reverse_video_cursor = true,
 
-config = utils.merge_all(config, keys)
+  color_scheme = "Kanagawa (Gogh)",
+  set_environment_variables = {
+    COLORSCHEME_FISH = "kanagawa.fish",
+    COLORSCHEME_NVIM = "kanagawa",
+  },
+
+  leader = {
+    key = "a",
+    mods = "CTRL",
+    timeout_milliseconds = 1000,
+  },
+  keys = {
+    {
+      key = "LeftArrow",
+      mods = "CTRL|SHIFT",
+      action = act.ActivateTabRelative(-1),
+    },
+    {
+      key = "RightArrow",
+      mods = "CTRL|SHIFT",
+      action = act.ActivateTabRelative(1),
+    },
+    {
+      key = "-",
+      mods = "LEADER",
+      action = act.SplitPane({
+        direction = "Down",
+        size = { Percent = 25 },
+      }),
+    },
+    {
+      key = "\\",
+      mods = "LEADER",
+      action = act.SplitHorizontal({
+        domain = "CurrentPaneDomain",
+      }),
+    },
+  },
+}
+
+wezterm.on("gui-startup", function(cmd)
+  local tab, pane, window = mux.spawn_window(cmd or {})
+  pane:split({
+    size = 0.4,
+    direction = "Bottom",
+  })
+  window:spawn_tab({})
+end)
 
 return config
