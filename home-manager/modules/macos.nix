042e3b848cdd0a1111c109a8b6d56557569a72b0
Gregory Gaskill <gregory@chronon.com>
2023-11-23 11:25:08 -0500

    Add smerge symlink

--- home-manager/modules/macos.nix
+++ home-manager/modules/macos.nix
@@ -17,6 +17,9 @@
       "${config.home.homeDirectory}/.1password/agent.sock" = {
         source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock";
       };
+      "${config.home.homeDirectory}/bin/smerge" = {
+        source = config.lib.file.mkOutOfStoreSymlink "/Applications/Sublime Merge.app/Contents/SharedSupport/bin/smerge";
+      };
     };
   };
 
