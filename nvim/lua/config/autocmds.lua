3d03323b92aabdebb4bc5c26e4af0d500f0fbc7e
Gregory Gaskill <gregory@chronon.com>
2024-08-14 11:08:15 -0400

    Remove no longer necessary autocommands

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,24 +1,10 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 
-vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = { "php", "javascript" },
-  callback = function()
-    vim.b.autoformat = false
-  end,
-})
-
 vim.api.nvim_create_autocmd("FileType", {
   pattern = { "gitcommit", "markdown" },
   callback = function()
     require("cmp").setup.buffer({ enabled = false })
-  end,
-})
-
-vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = { "nix" },
-  callback = function()
-    vim.opt_local.commentstring = "# %s"
   end,
 })
 
