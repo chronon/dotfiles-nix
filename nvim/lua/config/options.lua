f6e66390637a4bdc9776353a19def33881527e96
Gregory Gaskill <gregory@chronon.com>
2024-12-20 14:04:18 -0500

    Cleanup options, update blink config

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,14 +1,14 @@
+-- see https://www.lazyvim.org/configuration/general for defaults
+
 vim.g.mapleader = ","
 vim.opt.clipboard = ""
 vim.opt.cursorline = false
-vim.opt.foldmethod = "manual"
 vim.opt.linebreak = true
 vim.opt.list = false
 vim.opt.relativenumber = false
 vim.opt.shell = "fish"
-vim.opt.timeoutlen = 500
 vim.opt.undofile = false
 vim.opt.wrap = true
-vim.wo.conceallevel = 0
 
 vim.g.lazyvim_php_lsp = "intelephense"
+vim.g.snacks_animate = false
--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -2,9 +2,6 @@ return {
   {
     "saghen/blink.cmp",
     opts = {
-      highlight = {
-        use_nvim_cmp_as_default = true,
-      },
       completion = {
         menu = {
           auto_show = false,
