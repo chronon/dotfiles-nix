f79d585b6d309455fe312054f164a48a6886474f
Gregory Gaskill <gregory@chronon.com>
2026-03-05 08:11:31 -0500

    Update lockfiles and package name

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -2,6 +2,6 @@ return {
   { "folke/flash.nvim", enabled = false },
   { "folke/persistence.nvim", enabled = false },
   { "ggandor/flit.nvim", enabled = false },
-  { "ggandor/leap.nvim", enabled = false },
+  { "https://codeberg.org/andyg/leap.nvim.git", enabled = false },
   { "rafamadriz/friendly-snippets", enabled = false },
 }
