c5e96d8cd7d573d42484358a7022d36dc330afe1
Gregory Gaskill <gregory@chronon.com>
2026-03-11 09:44:29 -0400

    Disable hard wrap for markdown

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,11 +1,12 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 
--- vim.api.nvim_create_autocmd("FileType", {
---   pattern = { "gitcommit", "markdown" },
---   callback = function()
---     require("cmp").setup.buffer({ enabled = false })
---   end,
--- })
-
 vim.api.nvim_del_augroup_by_name("lazyvim_highlight_yank")
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+    vim.opt_local.textwidth = 0
+    vim.opt_local.formatoptions:remove({ "t", "c" })
+  end,
+})
