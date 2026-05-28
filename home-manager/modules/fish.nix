becf4b398d770d517db2215f0cffd8fc251391d8
Gregory Gaskill <gregory@chronon.com>
2026-05-28 08:14:09 -0400

    Fix pnpm v11 path, remove package

--- home-manager/modules/fish.nix
+++ home-manager/modules/fish.nix
@@ -52,7 +52,7 @@
         fish_add_path /usr/local/bin
         fish_add_path /opt/homebrew/bin
         fish_add_path "${config.home.homeDirectory}/.local/bin"
-        fish_add_path "${config.home.homeDirectory}/.local/share/pnpm"
+        fish_add_path "${config.home.homeDirectory}/.local/share/pnpm/bin"
       '';
 
       frg.body = ''
