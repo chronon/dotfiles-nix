acf42541f510e799be0558cd9a4918aeb6dc5611
Gregory Gaskill <gregory@chronon.com>
2026-06-01 14:19:01 -0400

    Fix frg after pushing esc

--- home-manager/modules/fish.nix
+++ home-manager/modules/fish.nix
@@ -58,7 +58,7 @@
       frg.body = ''
         rg --smart-case --color=always --line-number --no-heading "$argv" |
           fzf --ansi \
-            --color 'hl:-1:underline,hl+:-1:underline:reverse' \
+            --color 'dark,hl:-1:underline,hl+:-1:underline:reverse' \
             --delimiter ':' \
             --preview "bat --color=always {1} --highlight-line {2}" \
             --preview-window 'up,60%,border-bottom,+{2}+3/3,~3' \
