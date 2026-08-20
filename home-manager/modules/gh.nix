0e93ed2ecec357bfcd02a528f0bc0a37ea748f42
Gregory Gaskill <gregory@chronon.com>
2026-08-20 10:13:25 -0400

    Add gh botreview alias

--- home-manager/modules/gh.nix
+++ home-manager/modules/gh.nix
@@ -16,6 +16,24 @@
         prcd = "pr create --assignee @me --label dependencies";
         prm = "!gh pr merge --merge --delete-branch && git mp";
         rlp = "run list -s in_progress";
+        botreview = ''
+          !set -e
+          pr=''${1:-$(gh pr view --json number -q .number)}
+          [ -n "$pr" ] || exit 1
+
+          gh api --paginate "repos/{owner}/{repo}/pulls/$pr/reviews" --jq '
+            .[]
+            | select(.user.type == "Bot")
+            | select((.body // "") != "")
+            | "[\(.user.login)] \(.state)\n\(.body)\n"
+          '
+
+          gh api --paginate "repos/{owner}/{repo}/pulls/$pr/comments" --jq '
+            .[]
+            | select(.user.type == "Bot")
+            | "[\(.user.login)]\(if .position then "" else " (outdated)" end) \(.path):\(.line // .original_line)\n\(.body)\n"
+          '
+        '';
       };
     };
   };
